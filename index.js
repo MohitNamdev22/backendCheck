b20081afd7c886a9529175e00e1d5c375a443fd6
Mohit Namdev <namdevmohit0@gmail.com>
2024-01-01 13:29:08 +0530

    data sending using body

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express')
+var bodyParser = require('body-parser')
 const app = express()
 const port = 3000
 
@@ -10,8 +11,18 @@ function calculateSum(counter){
     return sum;
 }
 
+// function middleware1(req,res,next){
+//     console.log("from middleware "+ req.headers.counter);
+//     res.send("Error")
+// }
+
+// app.use(middleware1);
+
+app.use(bodyParser.json());
+
 function handleFirstRequest(req,res){
-    var counter = req.query.counter;
+    console.log(req.body) ;
+    var counter = req.body.counter;
     var calculatedSum = calculateSum(counter);
     console.log(calculatedSum)
     var answer = "the sum is " +calculatedSum;
@@ -22,8 +33,8 @@ function handleUser(req,res){
     res.send("hello");
 }
 
-app.get('/handleSum',handleFirstRequest)
-app.post('/handleUser',handleUser)
+//app.get('/handleSum',handleFirstRequest)
+app.post('/handleFirstRequest',handleFirstRequest)
 
 function started(){
     console.log(`Example app listening on port ${port}`)
