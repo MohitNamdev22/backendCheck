0fcb3d41858da39f0bacb6e52c35c751e4798760
Mohit Namdev <namdevmohit0@gmail.com>
2024-01-01 13:59:04 +0530

    html response

--- index.js
+++ index.js
@@ -46,8 +46,24 @@ function handleUser(req,res){
     res.send("hello");
 }
 
+function givePage(req,res){
+    res.send(`<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Hello fgrom page</title>
+    </head>
+    <body>
+        <b>Helloo</b>
+    </body>
+    </html>`)
+}
+
+
 //app.get('/handleSum',handleFirstRequest)
 app.post('/handleFirstRequest',handleFirstRequest)
+app.get('/',givePage)
 
 function started(){
     console.log(`Example app listening on port ${port}`)
